3a8650f07fe2a7691c5d88dd90486ddbcf5b64b4
VTIvanov20 <vtivanov20@codingburgas.bg>
2023-05-29 23:56:20 +0300

    Refactor app basis  * Implement navigation bar  * Add basic animations  * Add sample icons  * Update reun method

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,93 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+/// Flutter code sample for [BottomNavigationBar].
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const BottomNavigationBarExampleApp());
+
+class BottomNavigationBarExampleApp extends StatelessWidget {
+  const BottomNavigationBarExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: const MyHomePage(),
-      ),
+    return const MaterialApp(
+      home: BottomNavigationBarExample(),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Business',
+      style: optionStyle,
+    ),
+    Text(
+      'School',
+      style: optionStyle,
+    ),
+    Text(
+      'Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
     return Scaffold(
-      backgroundColor: Colors.amberAccent,
-      body: Column(
-        // children: [
-        //   const Text("Hey, Bob",
-        //       style: TextStyle(
-        //         color: Colors.white,
-        //         fontSize: 31,
-        //       )),
-        //   const Text("What are you in the mood for?",
-        //       style: TextStyle(
-        //         color: Colors.black,
-        //         fontSize: 17,
-        //       )),
-        //   Text(appState.current.asLowerCase),
-        // ],
-        children: [
-          Align(alignment: Alignment.topLeft, child: [
-            const Text("Hey, Bob",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 31,
-                )),
-            const Text("What are you in the mood for?",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 17,
-                )),
-            Text(appState.current.asLowerCase),
-          ])
+      appBar: AppBar(
+        title: const Text('Movie App'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.black87,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.black87,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.black87,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.black87,
+          ),
         ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.yellow[700],
+        onTap: _onItemTapped,
       ),
     );
   }
