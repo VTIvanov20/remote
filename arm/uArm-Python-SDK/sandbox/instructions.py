10f4afea169db0e221431ba62ca3ab524dae1c81
VTIvanov20 <vtivanov20@codingburgas.bg>
2023-06-18 15:13:32 +0300

    Add basic hardware communication script

---
+++ arm/uArm-Python-SDK/sandbox/instructions.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('..')
+from uarm.wrapper import SwiftAPI
+
+# todo: document which port is needed for the user (device manager -> ports -> COM port number (usually 3 or 4))
+swift = SwiftAPI(port="COM3", callback_thread_pool_size=1)
+
+swift.set_position(20, 0, 20)
+swift.reset()
+swift.set_position(150, 0, 150)
+
+print(swift.get_position())
