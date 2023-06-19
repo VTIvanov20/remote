0c0d5b47dae8cf624b5b96d371cf2c83068198ce
VTIvanov20 <vtivanov20@codingburgas.bg>
2023-06-19 23:15:46 +0300

    Include dartpy  * chore: finish inclusion

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:io'; // unstable, remove later
+import 'dart:ffi';
+import 'package:dartpy/dartpy.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/track_pad.dart';
 import 'package:movie_app/options.dart';
