a7c03ad44c65f2f6bcd02b346559e402c8d90eb4
VTIvanov20 <VTIvanov20@codingburgas.bg>
2023-06-26 12:08:26 +0100

    Freeze hardware-related files due to remote work

--- app/lib/arm_api.dart
+++ app/lib/arm_api.dart
@@ -1,23 +1,23 @@
-import 'package:dartpy/dartpy.dart';
+// import 'package:dartpy/dartpy.dart';
 
-class Vector3 {
-    final double x, y, z;
+// class Vector3 {
+//     final double x, y, z;
 
-    Vector3({required this.x, required this.y, required this.z});
-}
+//     Vector3({required this.x, required this.y, required this.z});
+// }
 
 
-class ArmApi {
-    int? port;
-    int? baudrate;
-    int? timeout;
+// class ArmApi {
+//     int? port;
+//     int? baudrate;
+//     int? timeout;
 
-    ArmApi({this.port, this.baudrate, this.timeout}) {
-        pyStart();
-    }
+//     ArmApi({this.port, this.baudrate, this.timeout}) {
+//         pyStart();
+//     }
 
-    static final Finalizer<void> _finalizer = Finalizer((p0) { pyCleanup(); });
+//     static final Finalizer<void> _finalizer = Finalizer((p0) { pyCleanup(); });
 
-    // Vector3 get position => {};
-    // set position()
-}
+//     // Vector3 get position => {};
+//     // set position()
+// }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:dartpy/dartpy.dart';
-import 'package:dartpy/dartpy_annotations.dart';
-part 'gen_dartpy_example.g.dart';
+// import 'package:dartpy/dartpy.dart';
+// import 'package:dartpy/dartpy_annotations.dart';
+// part 'gen_dartpy_example.g.dart';
 
 import 'package:flutter/material.dart';
 import 'package:movie_app/track_pad.dart';
 import 'package:movie_app/options.dart';
 import 'package:movie_app/button_layout.dart';
-import 'package:movie_app/script_trigger.dart';
+// import 'package:movie_app/script_trigger.dart';
 
 void main() => runApp(const ArmController());
 
@@ -49,7 +49,7 @@ class AppContents extends StatelessWidget {
           ),
         ),
         OptionBar(),
-        PyScript(), // unstable, remove later
+        // PyScript(), // unstable, remove later
         TrackPad(),
         ButtonLayout(),
       ],
