eedd896cb09b860fe01fd3c333658fd1a03556ad
VTIvanov20 <VTIvanov20@codingburgas.bg>
2023-07-18 14:34:52 +0300

    Start working on landscape UI layout

--- app/lib/views/landscape.dart
+++ app/lib/views/landscape.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-// import 'package:movie_app/widgets/track_pad.dart';
-// import 'package:movie_app/widgets/options.dart';
-// import 'package:movie_app/widgets/button_layout.dart';
-// import 'dart:io';
+import 'package:movie_app/widgets/track_pad.dart';
+import 'package:movie_app/widgets/options.dart';
+import 'package:movie_app/widgets/button_layout.dart';
+import 'dart:io';
 import 'package:movie_app/widgets/colors.dart';
 
 class LandscapeView extends StatefulWidget {
@@ -13,27 +13,26 @@ class LandscapeView extends StatefulWidget {
 }
 
 class _LandscapeViewState extends State<LandscapeView> {
-  // double x = 0;
-  // double y = 0;
+  double x = 0;
+  double y = 0;
 
-  // Future<void> sendCommand(String command, double x, double y, double z) async {
-  //   final socket = await Socket.connect('localhost', 12345);
-  //   print('Connected to Python script.');
+  Future<void> sendCommand(String command, double x, double y, double z) async {
+    final socket = await Socket.connect('localhost', 12345);
+    print('Connected to Python script.');
 
-  //   final data = '$command,$x,$y,$z';
-  //   socket.write(data);
+    final data = '$command,$x,$y,$z';
+    socket.write(data);
 
-  //   socket.close();
-  // }
+    socket.close();
+  }
 
   @override
   Widget build(BuildContext context) {
 
   return ListView(
-      physics: const NeverScrollableScrollPhysics(),
-      children: const [
-        Text(
-            "This is the landscape view",
+      children: [
+        const Text(
+            "Welcome",
             style: TextStyle(
               fontFamily: "Montserrat",
               fontSize: 17.5,
@@ -42,7 +41,22 @@ class _LandscapeViewState extends State<LandscapeView> {
             ),
             textAlign: TextAlign.center,
           ),
-      ]
+        const OptionBar(),
+        GestureDetector(
+          onPanUpdate: (details) {
+            setState(() {
+              x += details.delta.dx;
+              y += details.delta.dy;
+            });
+          },
+          child: TrackPad(
+            onPositionSelected: (x, y, z) {
+              sendCommand('position', x, y, z);
+            },
+          ),
+        ),
+        const ButtonLayout(),
+      ],
     );
   }
 }
